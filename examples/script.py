5b8caf2cd714b940e52288e1205d7b3d2aa5f392
jkk5454 <zyk5454@gmail.com>
2023-07-06 19:44:46 +0900

    keras and folder create

--- examples/script.py
+++ examples/script.py
@@ -13,10 +13,35 @@ def main():
     parser = argparse.ArgumentParser(description='MultiDDPG ')
     parser.add_argument('--train', type=int, default=1)
     args = parser.parse_args()
+    folder_path = "./data"
+    
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print("Directory " , folder_path ,  " Created ")
+    else:
+        print("Directory " , folder_path ,  " already exists")
     
     if args.train == 1:
+        folder_path = "./data/train"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            print("Directory " , folder_path ,  " Created ")
+        else:
+            print("Directory " , folder_path ,  " already exists")
+        folder_path = "./data/train/test"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            print("Directory " , folder_path ,  " Created ")
+        else:
+            print("Directory " , folder_path ,  " already exists")
         run_ddpg()
     else:
+        folder_path = "./data/test"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            print("Directory " , folder_path ,  " Created ")
+        else:
+            print("Directory " , folder_path ,  " already exists")
         test_ddpg()
 
 
--- softgym/softgym/envs/flex_env.py
+++ softgym/softgym/envs/flex_env.py
@@ -7,7 +7,6 @@ from softgym.utils.visualization import save_numpy_as_gif
 import cv2
 import os.path as osp
 import pickle
-from memory_profiler import profile
 
 try:
     import pyflex
